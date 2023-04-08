5554a7dd1243b1b5eb31d96fff1caabeace3b9f2
sergeyzelentsov <_sharapov_@mail.ru>
2023-04-08 14:27:27 +0300

    solution Task Code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-
+        printStringArray(args);
+        String [] result = new String[0];
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].length()<=3) {
+                String [] array = new String [result.length+1];
+                if (result.length>0){
+                    for (int j = 0; j < result.length; j++) {
+                        array[j] = result[j];
+                    }
+                }
+                array[result.length] = args[i];
+                result = array;
+            }
+        }
+        printStringArray(result);
+    }
+    public static void printStringArray(String[] array){
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if(i!= array.length-1){
+                System.out.print(", ");
+            }
+        }
+        System.out.print("]\r\n");
     }
 }
